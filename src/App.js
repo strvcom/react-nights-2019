c175678a6a1fb4cea2aaf29ad4c8b92efc409241
dannytce <dankijkov@gmail.com>
2019-03-10 18:20:45 +0100

    Add config, getToken, getProducts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
 import React, { Component } from 'react'
+import config from './config'
+
+const getToken = async () => {
+  const res = await fetch(`${config.apiUrl}/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      grant_type: 'client_credentials',
+      client_id: config.clientId,
+      scope: config.scope,
+    }),
+  })
+
+  return res.json()
+}
+
+const getProducts = async access_token => {
+  const res = await fetch(`${config.apiUrl}/api/skus`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  })
+
+  return res.json()
+}
+
 class App extends Component {
+  state = {
+    isLoading: true,
+    products: {},
+  }
+  async componentDidMount() {
+    const { access_token } = await getToken()
+    const products = await getProducts(access_token)
+
+    this.setState({
+      isLoading: false,
+      products,
+    })
+  }
+
   render() {
-    return <h1>Krutoprisna E-Commerce app</h1>
+    const {
+      isLoading,
+      products: { data },
+    } = this.state
+
+    return (
+      <div>
+        <h1>Krutoprisna E-Commerce app</h1>
+        {isLoading && <div>Loading ...</div>}
+        {data && (
+          <ul>
+            {data.map(({ attributes: item }, id) => (
+              <li key={id}>{item.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
   }
 }
 
