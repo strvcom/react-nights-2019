fbc42c9d5bd34112942c77a8890af58121ae9cde
dannytce <dankijkov@gmail.com>
2019-03-10 22:10:58 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    '@strv/eslint-config-react/v16',
+    '@strv/eslint-config-react/optional',
+    // '@strv/eslint-config-react/style',
+    /*
+      There is an error in there:
+          Configuration for rule "react/destructuring-assignment" is invalid:
+          Value {"ignoreClassFields":true} should be string.
+      Before fixed, let's keep it commented.
+    */
+    'prettier',
+    'prettier/react',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+}
