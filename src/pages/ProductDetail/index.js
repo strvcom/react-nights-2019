27f37bc68a21834a72e3731de9ad014444629cd6
Pavel Prichodko <prichodko.p@gmail.com>
2019-04-02 11:09:26 +0200

    Implement fetching in ProductDetail

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-// TODO: fetch product detail
+import Layout from '../../components/Layout'
+import Loader from '../../components/Loader'
+import { H1 } from '../../components/Typography'
+import { getProductById } from '../../api/get-product'
+
 class ProductDetail extends Component {
-  state = {}
+  state = {
+    product: null,
+  }
+
+  fetchProduct = async productId => {
+    this.setState({ isLoading: true })
+    const product = await getProductById(productId)
+    this.setState({ isLoading: false, product })
+  }
+
+  componentDidMount() {
+    const { productId } = this.props.match.params
+    this.fetchProduct(productId)
+  }
+
+  componentDidUpdate(prevProps) {
+    const { productId } = this.props.match.params
+    if (prevProps.match.params.productId !== productId) {
+      this.fetchProduct(productId)
+    }
+  }
+
   render() {
-    const { match } = this.props
+    const { isLoading, product } = this.state
+
     return (
-      <div>
-        <h1>Product detail: {match.params.productId}</h1>
-      </div>
+      <Layout>
+        {isLoading && <Loader />}
+        {product && <H1>{product.attributes.name}</H1>}
+        <Link to="/">Back</Link>
+      </Layout>
     )
   }
 }
