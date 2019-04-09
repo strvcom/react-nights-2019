cb1b2297c1980c1fe9a7b3ac4f1e8b7a13afeb6b
Pavel Prichodko <prichodko.p@gmail.com>
2019-04-09 18:11:45 +0200

    Add call to get customer token

--- src/api/customers/create-customer.js
+++ src/api/customers/create-customer.js
@@ -1,4 +1,6 @@
 import { api } from '../api-client'
+import config from '../../config'
+import { setToken } from '../../utils/token'
 
 export const createCustomer = async ({ email, password, firstName }) => {
   const requestBody = {
@@ -23,6 +25,19 @@ export const createCustomer = async ({ email, password, firstName }) => {
     const {
       data: { attributes },
     } = response
+
+    const { access_token } = await api('/oauth/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        grant_type: 'password',
+        username: email,
+        password: password,
+        client_id: config.clientId,
+      }),
+    })
+
+    setToken(access_token)
+
     return {
       email: attributes.email,
       firstName: attributes.metadata.firstName,
