dbbcb4e5c1d6d8da954a8d8c844d4b40ebbf3cc8
Erik Majlath <erikmajlath@gmail.com>
2019-04-17 18:35:14 +0200

    Install redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
-import { createStore, combineReducers } from 'redux'
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import cart from './cart/reducer'
 import customer from './customer/reducer'
 
@@ -8,11 +8,9 @@ const reducer = combineReducers({
   customer,
 })
 
+// this variable will be set if you have redux-dev-tools extension installed in your browser
+// https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export const configureStore = (preloadedState = {}) =>
-  createStore(
-    reducer,
-    preloadedState,
-    // this variable will be set if you have redux-dev-tools extension installed in your browser
-    // https://github.com/zalmoxisus/redux-devtools-extension#11-basic-store
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)))
