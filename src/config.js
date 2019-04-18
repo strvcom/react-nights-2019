1801fe0277f57ddb690bd326ff01ef35b1ddd09c
Erik Majlath <erikmajlath@gmail.com>
2019-04-18 15:51:08 +0200

    Change config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
-  clientId: '1639340def6563ae1342dca16e5e9711e696ffe45f1c21fe4fba8e272a03f51a',
-  scope: 'market:335',
-  apiUrl: 'https://the-amber-brand-12.commercelayer.io',
+  clientId: '1add790e25ab4f3b0724b593a09e1d1008fcce751d4c2b2f337353051f439eda',
+  clientSecret:
+    'eac6464b9c4840222b1258732cb51c5db35ab472be4ba305b24813c8c0076839',
+  scope: 'market:710',
+  apiUrl: 'https://the-brown-brand-23.commercelayer.io',
 }
