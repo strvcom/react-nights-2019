3642e2e0e5f4cd7fbc09305044602652224bafac
Michal Jarnot <m.jarnot@yahoo.com>
2019-04-21 16:19:47 +0200

    Create basic test user account page

--- src/pages/Account/test/index.test.js
+++ src/pages/Account/test/index.test.js
@@ -4,6 +4,7 @@ import 'jest-styled-components'
 import { App } from '../../../App'
 import * as routes from '../../../routes'
 import { renderWithRouter } from '../../../utilsTest/render'
+import { configureStore } from '../../../store'
 
 describe('[pages] Account', () => {
   describe('when not logged in', () => {
@@ -11,6 +12,27 @@ describe('[pages] Account', () => {
       const renderer = renderWithRouter(<App />, routes.ACCOUNT)
       const HTMLDivElement = renderer.getByTestId('login-page')
       expect(HTMLDivElement).toBeTruthy()
+    })
+  })
+
+  describe('when logged in', () => {
+    it('should render private user account page', async () => {
+      const store = configureStore({
+        customer: {
+          attributes: {
+            metadata: {
+              firstName: 'Michal',
+            },
+          },
+        },
+      })
+
+      const renderer = renderWithRouter(
+        <App customStore={store} />,
+        routes.ACCOUNT
+      )
+      const H1HtmlElement = renderer.getByText('Welcome Michal')
+      expect(H1HtmlElement).toBeTruthy()
     })
   })
 })
