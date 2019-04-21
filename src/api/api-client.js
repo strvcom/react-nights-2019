775bbc8014155e52ad2209be3b8a78b8bff6c19e
Erik Majlath <erikmajlath@gmail.com>
2019-04-21 12:18:08 +0200

    Finish api client

--- src/api/api-client.js
+++ src/api/api-client.js
@@ -4,59 +4,48 @@
 import config from '../config'
 import { getGuestToken } from './get-guest-token'
 import { refreshCustomerToken } from './customers/refresh-customer-token'
-import { getToken, removeToken } from '../utils/token'
+import { getToken } from '../utils/token'
 import { getRefreshToken } from '../utils/refresh-token'
 
+const makeRequest = (url, options, token) =>
+  fetch(`${config.apiUrl}${url}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/vnd.api+json',
+      Authorization: `Bearer ${token}`,
+    },
+    ...options,
+  })
+
 export const api = async (url, options) => {
-  // Let's retry the request maximum of 2 times
-  for (let x = 0; x < 2; x++) {
-    let token = getToken()
+  // Grab the token from the store or from the API
+  let token = getToken() || (await getGuestToken())
 
-    // If we dont have a token request a quest token
-    if (!token) {
-      token = await getGuestToken()
-    }
+  try {
+    // Do the request
+    let response = await makeRequest(url, options, token)
 
-    try {
-      // Do the request
-      const response = await fetch(`${config.apiUrl}${url}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/vnd.api+json',
-          Authorization: `Bearer ${token}`,
-        },
-        ...options,
-      })
+    // 401 unauthorized means we have expired token
+    if (response && response.status === 401) {
+      const refreshToken = getRefreshToken()
 
-      console.log('Got response', response)
-
-      // 401 unauthorized means we have expired token
-      if (response && response.status === 401) {
-        const refreshToken = getRefreshToken()
-        console.log('refresh token', refreshToken)
-
-        // If we have a refresh token this means we have logged in user
-        // and we need to refresh access token
-        if (refreshToken) {
-          console.log('awaiting refresh token')
-          await refreshCustomerToken()
-          console.log('refresh token received')
-          // We got the token let's request again
-          continue
-          // If no refresh token is present it means we had a expired quest token
-          // Guest tokens are acquired at the beignning of the request so let's
-          // just remove token and continue to the start of cycle
-        } else {
-          removeToken()
-          continue
-        }
+      // If we have a refresh token this means we have logged in user
+      // and we need to refresh access token
+      if (refreshToken) {
+        token = await refreshCustomerToken()
+      } else {
+        // If no refresh token is present just get new guest token
+        token = await getGuestToken()
       }
 
-      // If everything went fine just return the result
-      return response.json()
-    } catch (e) {
-      // TODO: redirect to login
-      console.log('redirecting')
+      // Repeat the request with the new token
+      response = await makeRequest(url, options, token)
     }
+
+    // If everything went fine just return the result
+    return response.json()
+  } catch (e) {
+    // TODO: redirect to login
+    console.log('redirecting')
   }
 }
