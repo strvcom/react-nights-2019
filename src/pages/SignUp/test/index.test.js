21ed32b1eb0c76759b1760fa431d6003e147873e
Michal Jarnot <m.jarnot@yahoo.com>
2019-04-21 17:22:33 +0200

    Write advanced tests for signup page
    - test unsuccessful submit
    - test successful submit

--- src/pages/SignUp/test/index.test.js
+++ src/pages/SignUp/test/index.test.js
@@ -1,14 +1,70 @@
 import React from 'react'
+import { fireEvent, waitForElement } from 'react-testing-library'
 import 'jest-styled-components'
 
 import { App } from '../../../App'
 import { renderWithRouter } from '../../../utilsTest/render'
 import * as routes from '../../../routes'
+import { mockSignUp201, mockSignUp422 } from '../../../utilsTest/mockHelpers'
+
+const fillInSignUpForm = renderer => {
+  const firstNameInput = renderer.container.querySelector(
+    'input[name="firstName"]'
+  )
+  const emailInput = renderer.container.querySelector('input[name="email"]')
+  const passwordInput = renderer.container.querySelector(
+    'input[name="password"]'
+  )
+  const passwordConfirmInput = renderer.container.querySelector(
+    'input[name="passwordConfirm"]'
+  )
+  const formSubmitButton = renderer.container.querySelector('button')
+
+  fireEvent.change(firstNameInput, { target: { value: 'michal' } })
+  fireEvent.change(emailInput, { target: { value: 'michal@test.com' } })
+  fireEvent.change(passwordInput, { target: { value: 'Heslo1234' } })
+  fireEvent.change(passwordConfirmInput, { target: { value: 'Heslo1234' } })
+
+  return { formSubmitButton }
+}
 
 describe('[pages] SignUp', () => {
   it('should render correctly', () => {
     const renderer = renderWithRouter(<App />, routes.SIGN_UP)
     const HTMLDivElement = renderer.getByTestId('signup-page')
     expect(HTMLDivElement).toBeTruthy()
+  })
+
+  describe('when form submit', () => {
+    describe('when error', () => {
+      it('should handle existing email error', async () => {
+        const renderer = renderWithRouter(<App />, routes.SIGN_UP)
+        const { formSubmitButton } = fillInSignUpForm(renderer)
+
+        mockSignUp422()
+
+        fireEvent.click(formSubmitButton)
+        const HTMLDivElement = await waitForElement(() =>
+          renderer.getByText('Email is already registered')
+        )
+
+        expect(HTMLDivElement).toBeTruthy()
+      })
+    })
+
+    describe('when success', () => {
+      it('should redirect user to account page', async () => {
+        const renderer = renderWithRouter(<App />, routes.SIGN_UP)
+        const { formSubmitButton } = fillInSignUpForm(renderer)
+
+        mockSignUp201()
+
+        fireEvent.click(formSubmitButton)
+        const HTMLDivElement = await waitForElement(() =>
+          renderer.getByText('Welcome Michal')
+        )
+        expect(HTMLDivElement).toBeTruthy()
+      })
+    })
   })
 })
--- src/utilsTest/mockHelpers.js
+++ src/utilsTest/mockHelpers.js
@@ -3,10 +3,47 @@ import config from '../config'
 import { PRODUCTS, PRODUCT } from './mockData'
 
 export const mockUnauthorizedRequest = () =>
-  fetchMock.post(`${config.apiUrl}/oauth/token`, {
+  fetchMock.restore().post(`${config.apiUrl}/oauth/token`, {
     status: 401,
     body: {},
   })
+
+export const mockAuthorizedRequest = () =>
+  fetchMock
+    .restore()
+    .post(`${config.apiUrl}/oauth/token`, {
+      status: 200,
+      body: {
+        access_token: 'accessToken',
+        token_type: 'bearer',
+        expires_in: 7200,
+        refresh_token: 'refreshToken',
+        scope: 'market:335',
+        created_at: 1555858950,
+        owner_id: 1,
+        owner_type: 'customer',
+      },
+    })
+    .get(`${config.apiUrl}/api/customers/1`, {
+      status: 200,
+      body: {
+        data: {
+          id: '1',
+          type: 'customers',
+          links: {
+            self: 'https://the-amber-brand-12.commercelayer.io/api/customers/1',
+          },
+          attributes: {
+            email: 'michal@test.com',
+            status: 'prospect',
+            created_at: '2019-04-21T15:02:29.631Z',
+            updated_at: '2019-04-21T15:02:29.631Z',
+            reference: null,
+            metadata: { firstName: 'Michal' },
+          },
+        },
+      },
+    })
 
 export const mockFetchProducts = () =>
   mockUnauthorizedRequest().get(`${config.apiUrl}/api/skus?include=prices`, {
@@ -18,4 +55,46 @@ export const mockFetchProduct = () =>
   mockUnauthorizedRequest().get(`${config.apiUrl}/api/skus/1?include=prices`, {
     status: 200,
     body: PRODUCT,
+  })
+
+export const mockSignUp422 = () =>
+  mockUnauthorizedRequest().post(`${config.apiUrl}/api/customers`, {
+    status: 411,
+    body: {
+      errors: [
+        {
+          title: 'has already been taken',
+          detail: 'email - has already been taken',
+          code: 'VALIDATION_ERROR',
+          source: { pointer: '/data/attributes/email' },
+          status: '422',
+          meta: {
+            error: 'taken',
+            value: 'test@test.com',
+          },
+        },
+      ],
+    },
+  })
+
+export const mockSignUp201 = () =>
+  mockAuthorizedRequest().post(`${config.apiUrl}/api/customers`, {
+    status: 201,
+    body: {
+      data: {
+        id: '1',
+        type: 'customers',
+        links: {
+          self: 'https://the-amber-brand-12.commercelayer.io/api/customers/1',
+        },
+        attributes: {
+          email: 'michal@test.com',
+          status: 'prospect',
+          created_at: '2019-04-21T15:02:29.631Z',
+          updated_at: '2019-04-21T15:02:29.631Z',
+          reference: null,
+          metadata: { firstName: 'Michal' },
+        },
+      },
+    },
   })
