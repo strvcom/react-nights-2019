6215985c75007ba658464c6b29e1aaba2a558e8f
Michal Jarnot <m.jarnot@yahoo.com>
2019-04-21 12:23:16 +0200

    Create basic button component snapshot

--- src/components/Button/test/index.test.js
+++ src/components/Button/test/index.test.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import 'jest-styled-components'
 
 import Button from '../index'
 
 describe('[components] Button', () => {
   it('should render correctly', () => {
     const renderer = render(<Button>My Button</Button>)
-    // If you want to preview the HTML
-    // renderer.debug()
-    expect(renderer.baseElement).toBeTruthy()
+    expect(renderer.baseElement).toMatchSnapshot()
   })
 })
