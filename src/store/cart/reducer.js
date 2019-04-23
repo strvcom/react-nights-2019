05aab106438c1b92fac20ea5c69ad46298b35b59
Arnost Pleskot <arnostpleskot@gmail.com>
2019-04-23 20:55:09 +0200

    Add product to cart one-liner

--- src/store/cart/reducer.js
+++ src/store/cart/reducer.js
@@ -1,13 +1,19 @@
 import omit from 'ramda/src/omit'
+import ifElse from 'ramda/src/ifElse'
+import has from 'ramda/src/has'
+import over from 'ramda/src/over'
+import lensProp from 'ramda/src/lensProp'
+import inc from 'ramda/src/inc'
+import assoc from 'ramda/src/assoc'
 import { ADD_PRODUCT, REMOVE_PRODUCT } from './actions'
+
+const addProductToCart = (prodId, state) =>
+  ifElse(has(prodId), over(lensProp(prodId), inc), assoc(prodId, 1))(state)
 
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case ADD_PRODUCT:
-      return {
-        ...state,
-        [action.payload]: (state[action.payload] || 0) + 1,
-      }
+      return addProductToCart(action.payload, state)
     case REMOVE_PRODUCT: {
       return omit([action.payload], state)
     }
