63b8fd47a041585cc264bbd3115db9058d2f39a5
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:25:07 +0200

    Fast forward to newest version of the file

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,63 +1,37 @@
-import React, { Component, Fragment } from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
-
-import * as customerActions from '../../store/customer/actions'
-import { removeToken } from '../../utils/token'
-import { removeCustomer } from '../../utils/customer'
+import * as routes from '../../routes'
 import { Wrapper, Header, HeaderSection, HeaderLink } from './styled'
 
-class Layout extends Component {
-  handleLogout = () => {
-    this.props.logout()
-    removeToken()
-    removeCustomer()
-    this.props.history.push('/')
-  }
-
-  render() {
-    const { isAuthenticated } = this.props
-
-    return (
-      <Fragment>
-        <Header>
-          <HeaderSection>
-            <HeaderLink to="/">All Products</HeaderLink>
-          </HeaderSection>
-          <HeaderSection>
-            <HeaderLink to="/cart">My Cart</HeaderLink>|
-            {isAuthenticated ? (
-              <>
-                <HeaderLink to="/account">My Account</HeaderLink>|
-                <HeaderLink as="button" onClick={this.handleLogout}>
-                  Logout
-                </HeaderLink>
-              </>
-            ) : (
-              <>
-                <HeaderLink to="/login">Log In</HeaderLink> |
-                <HeaderLink to="/signup">Sign Up</HeaderLink>
-              </>
-            )}
-          </HeaderSection>
-        </Header>
-        <Wrapper>{this.props.children}</Wrapper>
-      </Fragment>
-    )
-  }
-}
+const Layout = ({ isAuthenticated, children }) => (
+  <Fragment>
+    <Header>
+      <HeaderSection>
+        <HeaderLink href={routes.PRODUCT_LIST}>All Products</HeaderLink>
+      </HeaderSection>
+      <HeaderSection>
+        <HeaderLink href={routes.CART}>My Cart</HeaderLink>|
+        {isAuthenticated ? (
+          <>
+            <HeaderLink href={routes.ACCOUNT}>My Account</HeaderLink>|
+            <HeaderLink href={routes.LOGOUT} as="button">
+              Logout
+            </HeaderLink>
+          </>
+        ) : (
+          <>
+            <HeaderLink href={routes.LOGIN}>Log In</HeaderLink> |
+            <HeaderLink href={routes.SIGN_UP}>Sign Up</HeaderLink>
+          </>
+        )}
+      </HeaderSection>
+    </Header>
+    <Wrapper>{children}</Wrapper>
+  </Fragment>
+)
 
 const mapStateToProps = state => ({
   isAuthenticated: Object.keys(state.customer).length !== 0,
 })
 
-const mapDispatchToProps = {
-  logout: customerActions.logout,
-}
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Layout)
-)
+export default connect(mapStateToProps)(Layout)
