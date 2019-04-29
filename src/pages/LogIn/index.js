06166a856197a47b30121836d2eabf25a8637809
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:27:35 +0200

    Fast forward to newest version of the file

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -1,72 +1,69 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
 import { connect } from 'react-redux'
+import { toast } from 'react-toastify'
 
-import Layout from '../../components/Layout'
 import { H1 } from '../../components/Typography'
 import { Form, GlobalFormError } from '../../components/Form'
 import { Input } from '../../components/Input'
 import Button from '../../components/Button'
 import * as customerActions from '../../store/customer/actions'
-import { getCustomerToken } from '../../api/customers/get-customer-token'
-import { getCustomer } from '../../api/customers/get-customer'
+import { AsyncValidationError } from '../../utils/errors'
 import { schema } from './schema'
 
-class LogInPage extends Component {
-  state = {
-    globalError: '',
-  }
+const initialValues = {
+  email: '',
+  password: '',
+}
 
-  initialValues = {
-    email: '',
-    password: '',
-  }
+const LogInPage = ({ login }) => {
+  const [formAsyncError, setFormAsyncError] = useState('')
 
-  handleSubmit = async ({ email, password }, { setSubmitting }) => {
+  const handleSubmit = async ({ email, password }, { setSubmitting }) => {
     try {
       setSubmitting(true)
-      const { ownerId } = await getCustomerToken({
+
+      await login({
         username: email,
         password,
       })
-      const customer = await getCustomer(ownerId)
-      this.props.login(customer)
-      this.props.history.push('/account')
     } catch (error) {
-      this.setState({
-        globalError: error.message,
-      })
+      if (error instanceof AsyncValidationError) {
+        setFormAsyncError(error.message)
+      } else {
+        toast.error(
+          `There was an error while logging in, please try again later!`
+        )
+        // This would be nice place to log errors to some external service
+      }
+    } finally {
+      setSubmitting(false)
     }
-    setSubmitting(false)
   }
 
-  render() {
-    const { globalError } = this.state
-
-    return (
-      <Layout>
-        <H1 textAlign="center">Log In</H1>
-        <Formik
-          initialValues={this.initialValues}
-          validationSchema={schema}
-          onSubmit={this.handleSubmit}
-        >
-          {({ handleSubmit, isSubmitting }) => (
-            <Form onSubmit={handleSubmit}>
-              {Boolean(globalError) && (
-                <GlobalFormError>{globalError}</GlobalFormError>
-              )}
-              <Input name="email" type="email" label="Email address" />
-              <Input name="password" type="password" label="Password" />
-              <Button disabled={isSubmitting}>
-                {isSubmitting ? 'Logging In...' : 'Log In'}
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Layout>
-    )
-  }
+  return (
+    <>
+      <H1 textAlign="center">Log In</H1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        {props => (
+          <Form onSubmit={props.handleSubmit}>
+            {Boolean(formAsyncError) && (
+              <GlobalFormError>{formAsyncError}</GlobalFormError>
+            )}
+            <Input name="email" type="email" label="Email address" />
+            <Input name="password" type="password" label="Password" />
+            <Button disabled={props.isSubmitting}>
+              {props.isSubmitting ? 'Logging In...' : 'Log In'}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  )
 }
 
 const mapDispatchToProps = {
