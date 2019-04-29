95f141823a7fb235a45d16c0c2561658b0840332
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:32:48 +0200

    Rework store to support server initialization

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
-import { createStore, combineReducers } from 'redux'
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { getCustomer } from '../utils/customer'
 import products from './products/reducer'
 import cart from './cart/reducer'
 import customer from './customer/reducer'
+
+const isBrowser = typeof window !== 'undefined'
+const hasReduxDevTools = isBrowser && window.__REDUX_DEVTOOLS_EXTENSION__
+
+// this variable will be set if you have redux-dev-tools extension installed in your browser
+// https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = hasReduxDevTools
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose
 
 const reducer = combineReducers({
   products,
@@ -10,11 +20,13 @@ const reducer = combineReducers({
   customer,
 })
 
-export const configureStore = (preloadedState = {}) =>
-  createStore(
+export const configureStore = (preloadedState = {}) => {
+  return createStore(
     reducer,
-    preloadedState,
+    { ...preloadedState, customer: getCustomer() },
     // this variable will be set if you have redux-dev-tools extension installed in your browser
     // https://github.com/zalmoxisus/redux-devtools-extension#11-basic-store
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //hasReduxDevTools ? window.__REDUX_DEVTOOLS_EXTENSION__() : fn => fn
+    composeEnhancers(applyMiddleware(thunk))
   )
+}
