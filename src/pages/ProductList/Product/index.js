ef8ffd6220bc945a5906f2f3560085ee3db7a336
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:30:34 +0200

    Adjust to support next/link

--- src/pages/ProductList/Product/index.js
+++ src/pages/ProductList/Product/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { PRODUCT_LIST } from '../../../routes'
 import Button from '../../../components/Button'
+import { kebabCase } from '../../../utils/kebab-case'
 import { Wrapper, ImgWrap, Img, TitleWrap, Title, Price, Link } from './styled'
 
 const Product = ({ node, onAddToCart }) => (
   <Wrapper>
-    <Link to={node.id}>
+    <Link
+      href={`/product?id=${node.id}`}
+      as={`${PRODUCT_LIST}/${node.id}/${kebabCase(node.name)}`}
+    >
       <ImgWrap>
         <Img src={node.image_url} alt={`${node.name} image`} />
       </ImgWrap>
