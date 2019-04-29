4484453500a62a816053b9c13b1ceb941888e901
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:12:47 +0200

    Add routes file

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+export const HOMEPAGE = '/'
+export const PRODUCT_LIST = '/products'
+export const CART = '/cart'
+export const SIGN_UP = '/signup'
+export const LOGIN = '/login'
+export const ACCOUNT = '/account'
+export const LOGOUT = '/logout'
