ec3f88e96666e643ad68b11328db0ced76796acd
Lu Roman <xpynesoul@gmail.com>
2019-04-29 18:32:09 +0200

    Remove Layout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Button from '../../components/Button'
-import Layout from '../../components/Layout'
 import { H1 } from '../../components/Typography'
 import { removeProduct } from '../../store/cart/actions'
 
 class CartView extends Component {
   render() {
     return (
-      <Layout>
+      <>
         <H1>Your cart</H1>
         <ul>
           {this.props.items.map(item => (
@@ -26,7 +25,7 @@ class CartView extends Component {
             </li>
           ))}
         </ul>
-      </Layout>
+      </>
     )
   }
 }
