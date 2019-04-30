e586b9ecc5ed15b52f76ef7919730ac05dcce6f6
Lu Roman <xpynesoul@gmail.com>
2019-04-30 18:42:19 +0200

    Add isBrowser checks before calling functions

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,11 +1,13 @@
+import { isBrowser } from './is-browser'
+
 export const getToken = () => {
-  return window.localStorage.getItem('token')
+  return isBrowser() && window.localStorage.getItem('token')
 }
 
 export const setToken = token => {
-  window.localStorage.setItem('token', token)
+  isBrowser() && window.localStorage.setItem('token', token)
 }
 
 export const removeToken = () => {
-  window.localStorage.removeItem('token')
+  isBrowser() && window.localStorage.removeItem('token')
 }
