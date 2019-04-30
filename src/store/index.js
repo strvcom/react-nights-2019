a2d2876549cf0febade5dba52f631d549f34bae3
Lu Roman <xpynesoul@gmail.com>
2019-04-30 14:24:07 +0200

    Import isBrowser to replace local implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { getCustomer } from '../utils/customer'
+import { isBrowser } from '../utils/is-browser'
 import products from './products/reducer'
 import cart from './cart/reducer'
 import customer from './customer/reducer'
 
-const isBrowser = typeof window !== 'undefined'
-const hasReduxDevTools = isBrowser && window.__REDUX_DEVTOOLS_EXTENSION__
+const hasReduxDevTools = isBrowser() && window.__REDUX_DEVTOOLS_EXTENSION__
 
 // this variable will be set if you have redux-dev-tools extension installed in your browser
 // https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
