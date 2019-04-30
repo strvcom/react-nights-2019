ef85ab8b4c7639f1ea3e50b4a7836277f1042e65
Lu Roman <xpynesoul@gmail.com>
2019-04-30 14:21:01 +0200

    Add next's handle and return statements

--- server/index.js
+++ server/index.js
@@ -1,27 +1,23 @@
 /* eslint-env node */
 const express = require('express')
 const next = require('next')
-//const compression = require('compression')
-const { parse } = require('url')
 
 const PORT = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app
   .prepare()
   .then(() => {
     const server = express()
-    //server.use(compression())
 
     server.get('/products/:id/:name', (req, res) => {
-      app.render(req, res, '/product', req.params)
+      return app.render(req, res, '/product', req.params)
     })
 
     server.get('*', (req, res) => {
-      const parsedUrl = parse(req.url, true)
-      const { query, pathname } = parsedUrl
-      app.render(req, res, pathname, query)
+      return handle(req, res)
     })
 
     server.listen(PORT, err => {
