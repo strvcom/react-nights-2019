c65e1d74d07efe31a1098e2947d886d513c2d727
Lu Roman <xpynesoul@gmail.com>
2019-05-03 15:40:30 +0200

    Change to newer system to get styled components on server

--- pages/_document.js
+++ pages/_document.js
@@ -3,18 +3,47 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class CustomDocument extends Document {
-  render() {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
     return (
       <html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          {styleTags}
+          <meta charSet="utf-8" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content="#000000" />
+          <link rel="manifest" href="/static/manifest.json" />
         </Head>
+
         <body>
-          {main}
+          <Main />
           <NextScript />
         </body>
       </html>
