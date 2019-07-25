3cdfd02b07b9af70da8da391fd63021d7fa0dbd1
Dima An <dmitriym44@gmail.com>
2019-07-25 09:45:44 +0200

    Update constants.ts (#23)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const PAGE_SIZE_OPTIONS = [10, 25, 50, 100]
-export const PAGE_SIZE_DEFAULT = PAGE_SIZE_OPTIONS[2]
+export const PAGE_SIZE_OPTIONS = [5, 10, 25]
+export const PAGE_SIZE_DEFAULT = PAGE_SIZE_OPTIONS[1]
 export const PAGE_DEFAULT = 1
